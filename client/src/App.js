b4adb9547901ab4eaf6022d319025e8ed1447418
Carlton Joseph <carlton.joseph@gmail.com>
2020-03-14 20:28:21 -0700

    add a bar chart with recharts but found it limited

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,44 @@
 import React from "react";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Bar,
+  Legend
+} from "recharts";
 
 import dataRaw from "./data";
 
-const sites = [
-  "carltonathome",
-  "carltonwin8",
-  "tinaandcarlton",
-  "appsfortracking",
-  "apps4tracking",
-  "carltonjoseph",
-  "sophieandchampagne"
-];
-
-const urlsConfigs = {
-  local: {
-    transport: "http"
-  },
-  com: {
-    transport: "https"
-  }
-};
-
-const useLocal = false;
-const baseUrl = useLocal ? "local" : "com";
-const transport = urlsConfigs[baseUrl].transport;
-const url = site => `${transport}://${sites}.${baseUrl}`;
-
-const bitly = {};
-sites.map(s => (bitly[url(s)] = s));
-
-const data = dataRaw.map(s => {
-  return {
-    name: bitly[s.url]
-  };
+const data = dataRaw[0].data.results.map(s => {
+  const name = s.url.match(/https:\/\/([a-zA-Z0-9]+).com/)[1];
+  const max = s.time.reduce((a, d) => (d.delta > a ? d.delta : a), 0);
+  const min = s.time.reduce((a, d) => (d.delta < a ? d.delta : a), 50000);
+  const avgSumCount = s.time.reduce(
+    (a, d) => ({ sum: a.sum + d.delta, count: a.count + 1 }),
+    { sum: 0, count: 0 }
+  );
+  const avg = avgSumCount.sum / avgSumCount.count;
+  return { name, min, max, avg };
 });
 
 function App() {
-  console.log(dataRaw, data);
-  return <div>hi</div>;
+  return (
+    <div>
+      <h1>Bar Chart</h1>
+      <BarChart width={730} height={250} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="max" fill="#8884d8" />
+        <Bar dataKey="min" fill="#82ca9d" />
+        <Bar dataKey="avg" fill="#12cafc" />
+      </BarChart>
+    </div>
+  );
 }
 
 export default App;
